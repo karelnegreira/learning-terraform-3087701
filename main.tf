4f6fe76f3bac6268dcb5faea05a27b330d860bdc
karelnegreira <57069677+karelnegreira@users.noreply.github.com>
2026-03-27 16:57:25 +0400

    commit bug fix vpc zone identifier typed twice

--- main.tf
+++ main.tf
@@ -101,7 +101,6 @@ launch_template_name = "blog"
 min_size = 1
 max_size = 2
 
-vpc_zone_identifier = module.blog_vpc.public_subnets
 
 launch_template_name = "blog"
 security_groups      = [module.blog_sg.security_group_name]
