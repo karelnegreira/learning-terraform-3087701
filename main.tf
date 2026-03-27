af8170b33db610bfd21eb766bc7508b98c6cfa4e
karelnegreira <57069677+karelnegreira@users.noreply.github.com>
2026-03-27 16:54:30 +0400

    commit autoscaling

--- main.tf
+++ main.tf
@@ -34,18 +34,7 @@ module "blog_vpc" {
 }
 
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_name]
-  
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -98,3 +87,33 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_id        = aws_instance.blog.id
   port             = 80
 }
+
+module "blog_autoscaling" {
+source  = "terraform-aws-modules/autoscaling/aws"
+version = "9.2.0"
+
+name = "blog"
+
+vpc_zone_identifier = module.blog_vpc.public_subnets
+
+launch_template_name = "blog"
+
+min_size = 1
+max_size = 2
+
+vpc_zone_identifier = module.blog_vpc.public_subnets
+
+launch_template_name = "blog"
+security_groups      = [module.blog_sg.security_group_name]
+instance_type        = var.instance_type
+
+image_id             = data.aws_ami.app_ami.id
+
+traffic_source_attachments = {
+  blog-alb = {
+    traffic_source_identifier = aws_lb_target_group.blog.arn
+  }
+}
+}
+
+
